2859963e629552cf4f826d6c1694164b232c5e47
Fran Enguix <enguix.fco@gmail.com>
2022-08-18 23:30:46 +0200

    Shop block with facing orientation

--- src/main/java/com/retsal/snwy/common/block/ShopBlock.java
+++ src/main/java/com/retsal/snwy/common/block/ShopBlock.java
@@ -3,7 +3,7 @@ package com.retsal.snwy.common.block;
 import com.retsal.snwy.register.InitItems;
 import com.retsal.snwy.utility.CoinType;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -12,18 +12,23 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
 
 public class ShopBlock extends Block {
     /// Usage:
@@ -37,9 +42,11 @@ public class ShopBlock extends Block {
     ///   _boat
 
     private static final VoxelShape SHAPE = makeShape();
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public ShopBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(getStateDefinition().any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
@@ -52,6 +59,17 @@ public class ShopBlock extends Block {
         shape = Shapes.join(shape, Shapes.box(0, 0, 0, 1, 1, 1), BooleanOp.OR);
 
         return shape;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        return defaultBlockState().setValue(FACING, blockPlaceContext.getHorizontalDirection().getOpposite());
     }
 
     @Override
